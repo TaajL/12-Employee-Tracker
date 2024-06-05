706979bfaa703714f6b43063c08c0fdee91ab6a8
Malcom X <Taajlogan@gmail.com>
2024-06-05 15:56:22 -0400

    addedDepartmenttable

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS employee_tracker 
+CREATE DATABASE employee_tracker;
+
+USE employee_tracker;
+CREATE TABLE department (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
