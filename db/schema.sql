0b62d33879aedb9905d9441a6cb9332904f08c1b
Malcom X <Taajlogan@gmail.com>
2024-06-05 16:37:36 -0400

    addedroletable

--- db/schema.sql
+++ db/schema.sql
@@ -21,4 +21,22 @@ CREATE TABLE role (
         ON DELETE CASCADE
 );
 
+CREATE TABLE employee (
+id INT AUTO_INCREMENT
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT,
+manager_id INT,
+PRIMARY KEY(id),
+FOREIGN KEY(role_id)
+    REFERENCES role(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+FOREIGN KEY(manager_id)
+REFERENCES employee(id)
+ON UPDATE CASCADE
+ON DELETE SET NULL
+);
+
+
 
