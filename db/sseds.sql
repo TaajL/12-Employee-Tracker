653ea50afbf1ffe77a8338b5defd5b3ea82c393d
Malcom X <Taajlogan@gmail.com>
2024-06-05 19:43:28 -0400

    addDptSeedValues

---
+++ db/sseds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (name)
+VALUES
+    ("Head of IT")
+    ("Security Engineer Manage")
+    ("Network Engineer Manager")
+    ("Help Desk Manager")
+    ("Security Engineer")
+    ("IT Help Technician")
