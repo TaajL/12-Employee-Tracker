4ca7077a11136fb0ea0bc1dd9e7019bf171c2637
Malcom X <Taajlogan@gmail.com>
2024-06-05 19:52:13 -0400

    addedrolesvalues

--- db/sseds.sql
+++ db/sseds.sql
@@ -6,3 +6,12 @@ VALUES
     ("Help Desk Manager")
     ("Security Engineer")
     ("IT Help Technician")
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+    ("Head of IT", 500000.00, 1)
+    ("Security Engineer Manage", 350000.00, 2)
+    ("Network Engineer Manager", 350000.00, 3)
+    ("Help Desk Manager", 150000.00, 4)
+    ("Security Engineer", 200000.00, 5)
+    ("IT Help Technician", 85000.00, 6)
