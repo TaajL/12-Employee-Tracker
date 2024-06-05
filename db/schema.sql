177a9191bb3fbbeb7b8bff51873e8be8492135f5
Malcom X <Taajlogan@gmail.com>
2024-06-05 16:10:29 -0400

    addedROLEtable

--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,23 @@ DROP DATABASE IF EXISTS employee_tracker
 CREATE DATABASE employee_tracker;
 
 USE employee_tracker;
+
 CREATE TABLE department (
     id INT AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY(id)
 );
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT
+    PRIMARY KEY(id)
+    FOREIGN KEY(department_id)
+        REFERENCES department(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+
