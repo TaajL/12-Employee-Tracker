a45b9ac3832e81e85d0965f2937ad8d19a6366bd
Malcom X <Taajlogan@gmail.com>
2024-06-06 13:26:49 -0400

    addedtableheaders

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const db = mysql.createConnection({
     user: "root",
     password:"....",
     database: "employee_tracker";
-})
+});
 console.log("employee_tracker database connected")
-    
+
+const TABLE_HEADERS ={
+    DEPARTMENTS: ["Department ID", "Department Name"],
+    ROLES: ["Role ID, Job Title", "Department Name", "Salary"],
+    EMPLOYEES: ["Employee ID", "First Name", "Last Name", "Jobe Title"],
+};
+
+
 
