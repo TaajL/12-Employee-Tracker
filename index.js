18ed2bb487b819fd26068bdfa8782bec78728dcc
Malcom X <Taajlogan@gmail.com>
2024-06-06 13:16:33 -0400

    connecteddbVar

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 const table = require("cli-table3");
 
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password:"....",
+    database: "employee_tracker";
+})
+console.log("employee_tracker database connected")
+    
+
